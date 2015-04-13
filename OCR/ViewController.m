3b94a2dcdef4566b7ce92edaea6794f425118fcb
huijinghuang <dmsehuang@huijinghuangs-MacBook-Pro.local>
2015-04-12 22:20:15 -0700

    line detection done

--- OCR/ViewController.m
+++ OCR/ViewController.m
@@ -20,14 +20,17 @@
     
     // change the image name to your image
     //UIImage* img = [UIImage imageNamed:@"1_sample_complete.png"];
-    UIImage* img = [UIImage imageNamed:@"2_sample_part.png"];
-    //UIImage* img = [UIImage imageNamed:@"3_sample_color.png"];
+    //UIImage* img = [UIImage imageNamed:@"2_sample_part.png"];
+    UIImage* img = [UIImage imageNamed:@"3_sample_color.png"];
     //UIImage* img = [UIImage imageNamed:@"4_sample_jack-ma.png"];
     //UIImageView* initImgView = [[UIImageView alloc] initWithImage:img];
     //[self.view addSubview:initImgView];
+
     UIImage* BWImg = [self convertImageTOBlackNWhite:img];
-    UIImageView* BWImgView = [[UIImageView alloc] initWithImage:BWImg];
+    UIImageView* BWImgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 45, BWImg.size.width, BWImg.size.height)];
+    [BWImgView setImage:BWImg];
     [self.view addSubview:BWImgView];
+    //[self lineDetection:img];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -54,7 +57,9 @@
     return rawData;
 }
 
+// line detection core algorithm
 -(UIImage *)convertImageTOBlackNWhite:(UIImage *) image{
+    // step 2 threshold the image
     unsigned char* rawData = [self UIImageToRGBA8:image];
     CGFloat threshold = 0.5;
     for (int i = 0; i < image.size.width * image.size.height * 4; i += 4) {
@@ -66,6 +71,59 @@
             rawData[i] = 255;
             rawData[i+1] = 255;
             rawData[i+2] = 255;
+        }
+    }
+    
+    CGImageRef imageRef = [image CGImage];
+    NSUInteger width = CGImageGetWidth(imageRef);
+    NSUInteger height = CGImageGetHeight(imageRef);
+    NSUInteger bytesPerPixel = 4;
+    NSUInteger bytesPerRow = bytesPerPixel * width;
+    
+    // step 3 horizontal projection
+    NSMutableArray* lines = [NSMutableArray arrayWithCapacity:height];
+    bool top = false;
+    int* horiArr = (int*) calloc(height, sizeof(int));
+    for (int i = 0; i < height; i++) {
+        int blackPixel = 0;
+        for (int j = 0; j < width; j++) {
+            NSUInteger index = bytesPerRow * i + bytesPerPixel * j;
+            if (rawData[index] == 0) {
+                blackPixel++;
+                //NSLog(@"black");
+            }
+        }
+        horiArr[i] = blackPixel;
+        UIView* lineView = [[UIView alloc] initWithFrame:CGRectMake(200, i, horiArr[i], 1)];
+        lineView.backgroundColor = [UIColor blackColor];
+        [self.view addSubview:lineView];
+        
+        if (blackPixel > 0) {
+            if (!top || i == height-1) {
+                // last black pixel should also be in
+                [lines addObject:[NSNumber numberWithInt:i]];
+                // draw line to test
+                for (int j = 0; j < width; j++) {
+                    NSUInteger index = bytesPerRow * i + bytesPerPixel * j;
+                    rawData[index] = 255;
+                    rawData[index+1] = 102;
+                    rawData[index+2] = 102;
+                }
+                //NSLog(@"top: %d", i);
+                top = true;
+            }
+        } else {
+            if (top) {
+                //NSLog(@"bottom: %d", i);
+                [lines addObject:[NSNumber numberWithInt:i]];
+                for (int j = 0; j < width; j++) {
+                    NSUInteger index = bytesPerRow * i + bytesPerPixel * j;
+                    rawData[index] = 255;
+                    rawData[index+1] = 102;
+                    rawData[index+2] = 102;
+                }
+                top = false;
+            }
         }
     }
     
